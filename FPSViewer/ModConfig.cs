ea451209b1d573533d9f013c7352782b7339bd62
Adham084 <79041023+Adham084@users.noreply.github.com>
2023-07-26 13:32:26 +0300

    fixe default values

--- FPSViewer/ModConfig.cs
+++ FPSViewer/ModConfig.cs
@@ -1,14 +1,15 @@
 ﻿using Microsoft.Xna.Framework;
+using StardewValley;
 
 namespace FPSViewer
 {
-	public class ModConfig
-	{
+    public class ModConfig
+    {
+        public Vector2 TextPos = new(10);
+        public Color TextColor = Game1.textColor;
         public bool Enable { get; set; } = true;
-        public Vector2 TextPos  = new(10);
-        public Color TextColor  = Color.White;
-        public float TextSize { get; set; } = 1.0f;
-		public bool DrawShadow { get; set; } = true;
+        public int TextSize { get; set; } = 1;
+        public bool DrawShadow { get; set; } = true;
         public bool DrawBackground { get; set; } = true;
     }
 }
